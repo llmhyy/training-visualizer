86fe0ac9a7bef01864ee789cf12280515d7d99b3
Yang Xianglin <yxlyzxl@gmail.com>
2022-05-19 13:35:47 +0800

    refactor

--- server/server.py
+++ server/server.py
@@ -5,10 +5,8 @@
 # TODO change to timevis format
 # TODO set a base class for some trainer functions... we dont need too many hyperparameters for frontend
 from PIL import Image
-import matplotlib.pyplot as plt
 
-from unicodedata import name
-from flask import request, Response, Flask, jsonify, make_response
+from flask import request, Flask, jsonify, make_response
 from flask_cors import CORS, cross_origin
 import base64
 import os
@@ -18,16 +16,13 @@ import torch
 import pandas as pd
 import numpy as np
 
-# import tensorflow as tf
-# from umap.umap_ import find_ab_params
+from umap.umap_ import find_ab_params
 from sqlalchemy import create_engine, text
 
 from antlr4 import *
-# import MyGrammarLexer
-# import MyGrammarParser
-# import MyGrammarPrintListener
 
-sys.path.append("..")
+timevis_path = "../../DLVisDebugger"
+sys.path.append(timevis_path)
 from singleVis.SingleVisualizationModel import SingleVisualizationModel
 from singleVis.data import DataProvider
 from singleVis.eval.evaluator import Evaluator
@@ -82,8 +77,8 @@ def load():
     model = SingleVisualizationModel(input_dims=512, output_dims=2, units=256)
     negative_sample_rate = 5
     min_dist = .1
-    # _a, _b = find_ab_params(1.0, min_dist)
-    umap_loss_fn = UmapLoss(negative_sample_rate, DEVICE, _a=[1.0], _b=[1.0], repulsion_strength=1.0)
+    _a, _b = find_ab_params(1.0, min_dist)
+    umap_loss_fn = UmapLoss(negative_sample_rate, DEVICE, _a=_a, _b=_b, repulsion_strength=1.0)
     recon_loss_fn = ReconstructionLoss(beta=1.0)
     criterion = SingleVisLoss(umap_loss_fn, recon_loss_fn, lambd=LAMBDA)
 
@@ -94,78 +89,77 @@ def load():
     trainer.load(file_path=os.path.join(data_provider.model_path,"{}".format(VIS_MODEL_NAME)))
     trainer.model.eval()
 
-    # vis = visualizer(data_provider, trainer.model, RESOLUTION, 10, CLASSES)
     evaluator = Evaluator(data_provider, trainer)
     timevis = TimeVisBackend(data_provider, trainer, evaluator)
 
 
-    sql_engine       = create_engine('mysql+pymysql://xg:password@localhost/dviDB', pool_recycle=3600)
-    db_connection    = sql_engine.connect()
+    # sql_engine       = create_engine('mysql+pymysql://xg:password@localhost/dviDB', pool_recycle=3600)
+    # db_connection    = sql_engine.connect()
 
-    # Search the following tables in MYSQL database and drop them if they exist
-    sql_engine.execute(text('DROP TABLE IF EXISTS SubjectModel;'))
-    sql_engine.execute(text('DROP TABLE IF EXISTS VisModel;'))
-    sql_engine.execute(text('DROP TABLE IF EXISTS Sample;'))
-    sql_engine.execute(text('DROP TABLE IF EXISTS NoisySample;'))
-    sql_engine.execute(text('DROP TABLE IF EXISTS AlSample;'))
-    sql_engine.execute(text('DROP TABLE IF EXISTS PredSample;'))
+    # # Search the following tables in MYSQL database and drop them if they exist
+    # sql_engine.execute(text('DROP TABLE IF EXISTS SubjectModel;'))
+    # sql_engine.execute(text('DROP TABLE IF EXISTS VisModel;'))
+    # sql_engine.execute(text('DROP TABLE IF EXISTS Sample;'))
+    # sql_engine.execute(text('DROP TABLE IF EXISTS NoisySample;'))
+    # sql_engine.execute(text('DROP TABLE IF EXISTS AlSample;'))
+    # sql_engine.execute(text('DROP TABLE IF EXISTS PredSample;'))
 
-    # Create the SubjectModel table in MYSQL database and insert the data
-    table_subject_model = "SubjectModel"
-    data_subject_model = timevis.subject_model_table()
-    data_subject_model.to_sql(table_subject_model, db_connection, if_exists='fail');
+    # # Create the SubjectModel table in MYSQL database and insert the data
+    # table_subject_model = "SubjectModel"
+    # data_subject_model = timevis.subject_model_table()
+    # data_subject_model.to_sql(table_subject_model, db_connection, if_exists='fail');
 
-    # Create the VisModel table in MYSQL database and insert the data
-    table_vis_model = "VisModel"
-    data_vis_model = timevis.vis_model_table()
-    data_vis_model.to_sql(table_vis_model, db_connection, if_exists='fail');
+    # # Create the VisModel table in MYSQL database and insert the data
+    # table_vis_model = "VisModel"
+    # data_vis_model = timevis.vis_model_table()
+    # data_vis_model.to_sql(table_vis_model, db_connection, if_exists='fail');
 
-    # Create the Sample table in MYSQL database and insert the data
-    table_sample = "Sample"
-    data_sample = timevis.sample_table()
-    data_sample.to_sql(table_sample, db_connection, if_exists='fail');
+    # # Create the Sample table in MYSQL database and insert the data
+    # table_sample = "Sample"
+    # data_sample = timevis.sample_table()
+    # data_sample.to_sql(table_sample, db_connection, if_exists='fail');
 
-    # For nosiy or active learning data, currently not tested yet
-    if "noisy" in CONTENT_PATH:     
-        table_noisy_sample = "NoisySample"
-        data_noisy_sample = timevis.sample_table_noisy()
-        data_noisy_sample.to_sql(table_noisy_sample, db_connection, if_exists='fail');
-    elif "active" in CONTENT_PATH:
-        table_al_sample = "AlSample"
-        data_al_sample = timevis.sample_table_AL()
-        data_al_sample.to_sql(table_al_sample, db_connection, if_exists='fail');
+    # # For nosiy or active learning data, currently not tested yet
+    # if "noisy" in CONTENT_PATH:     
+    #     table_noisy_sample = "NoisySample"
+    #     data_noisy_sample = timevis.sample_table_noisy()
+    #     data_noisy_sample.to_sql(table_noisy_sample, db_connection, if_exists='fail');
+    # elif "active" in CONTENT_PATH:
+    #     table_al_sample = "AlSample"
+    #     data_al_sample = timevis.sample_table_AL()
+    #     data_al_sample.to_sql(table_al_sample, db_connection, if_exists='fail');
     
-    # Ablation starts here
-    # Store prediction, deltaboundary true/false for all samples in all epochs in PredSample table
-    all_prediction_list = []
-    all_deltab_list = []
-    all_epochs_list = []
-    all_idx_list = []
-    for iteration in range(data_provider.s, data_provider.e+1, data_provider.p):
-        print("iteration", iteration)
-        train_data = data_provider.train_representation(iteration)
-        test_data = data_provider.test_representation(iteration)
-        all_data = np.concatenate((train_data, test_data), axis=0)
+    # # Ablation starts here
+    # # Store prediction, deltaboundary true/false for all samples in all epochs in PredSample table
+    # all_prediction_list = []
+    # all_deltab_list = []
+    # all_epochs_list = []
+    # all_idx_list = []
+    # for iteration in range(data_provider.s, data_provider.e+1, data_provider.p):
+    #     print("iteration", iteration)
+    #     train_data = data_provider.train_representation(iteration)
+    #     test_data = data_provider.test_representation(iteration)
+    #     all_data = np.concatenate((train_data, test_data), axis=0)
 
-        prediction = data_provider.get_pred(iteration, all_data).argmax(-1)
-        deltab = data_provider.is_deltaB(iteration, all_data)
+    #     prediction = data_provider.get_pred(iteration, all_data).argmax(-1)
+    #     deltab = data_provider.is_deltaB(iteration, all_data)
 
-        count = 0
-        for idx,_ in enumerate(prediction):
-            all_prediction_list.append(prediction[idx])
-            all_deltab_list.append(deltab[idx])
-            all_epochs_list.append(iteration)
-            all_idx_list.append(count)
-            count += 1
+    #     count = 0
+    #     for idx,_ in enumerate(prediction):
+    #         all_prediction_list.append(prediction[idx])
+    #         all_deltab_list.append(deltab[idx])
+    #         all_epochs_list.append(iteration)
+    #         all_idx_list.append(count)
+    #         count += 1
 
-    data_pred_sample = pd.DataFrame(list(zip(all_idx_list, all_epochs_list, all_prediction_list, all_deltab_list)),
-               columns =['idx', 'epoch', 'pred', 'deltab'])
-    table_pred_sample = "PredSample"
-    data_pred_sample.to_sql(table_pred_sample, db_connection, if_exists='fail')
+    # data_pred_sample = pd.DataFrame(list(zip(all_idx_list, all_epochs_list, all_prediction_list, all_deltab_list)),
+    #            columns =['idx', 'epoch', 'pred', 'deltab'])
+    # table_pred_sample = "PredSample"
+    # data_pred_sample.to_sql(table_pred_sample, db_connection, if_exists='fail')
 
-    db_connection.close()
+    # db_connection.close()
 
-    # TODO have not finish loading all the points
+    # !TODO have not finish loading all the points
 
 
 @app.route('/updateProjection', methods=["POST", "GET"])
@@ -185,7 +179,7 @@ def update_projection():
     except:
         raise NameError("config file not exists...")
 
-    CLASSES = config["CLASSES"]
+    CLASSES = np.array(config["CLASSES"])
     DATASET = config["DATASET"]
     LAMBDA = config["TRAINING"]["LAMBDA"]
     EPOCH_START = config["EPOCH_START"]
@@ -195,7 +189,6 @@ def update_projection():
     VIS_MODEL_NAME = config["VISUALIZATION"]["VIS_MODEL_NAME"]
     RESOLUTION = config["VISUALIZATION"]["RESOLUTION"]
     EPOCH = EPOCH_START + (iteration - 1)* EPOCH_PERIOD
-
 
     # define hyperparameters
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -210,8 +203,8 @@ def update_projection():
     model = SingleVisualizationModel(input_dims=512, output_dims=2, units=256)
     negative_sample_rate = 5
     min_dist = .1
-    # _a, _b = find_ab_params(1.0, min_dist)
-    umap_loss_fn = UmapLoss(negative_sample_rate, DEVICE, _a=[1.0], _b=[1.0], repulsion_strength=1.0)
+    _a, _b = find_ab_params(1.0, min_dist)
+    umap_loss_fn = UmapLoss(negative_sample_rate, DEVICE, _a=_a, _b=_b, repulsion_strength=1.0)
     recon_loss_fn = ReconstructionLoss(beta=1.0)
     criterion = SingleVisLoss(umap_loss_fn, recon_loss_fn, lambd=LAMBDA)
 
@@ -226,30 +219,28 @@ def update_projection():
     evaluator = Evaluator(data_provider, trainer)
     timevis = TimeVisBackend(data_provider, trainer, evaluator)
 
-
     train_data = data_provider.train_representation(EPOCH)
     test_data = data_provider.test_representation(EPOCH)
     all_data = np.concatenate((train_data, test_data), axis=0)
 
-    embedding_2d = trainer.model.encoder(
-        torch.from_numpy(all_data).to(dtype=torch.float32, device=trainer.DEVICE)).cpu().detach().numpy().tolist()
-
+    embedding_2d = timevis.batch_project(all_data).tolist()
+    # embedding_2d = trainer.model.encoder(
+    #     torch.from_numpy(all_data).to(dtype=torch.float32, device=trainer.DEVICE)).cpu().detach().numpy().tolist()
     train_labels = data_provider.train_labels(EPOCH)
     test_labels = data_provider.test_labels(EPOCH)
     labels = np.concatenate((train_labels, test_labels), axis=0).tolist()
 
-    training_data_number = train_data.shape[0]
-    testing_data_number = test_data.shape[0]
+    training_data_number = config["TRAINING_LEN"]
+    testing_data_number = config["TESTING_LEN"]
     testing_data_index = list(range(training_data_number, training_data_number + testing_data_number))
 
+    # !TODO[fix] need to align background point position here for each epoch...
     grid, decision_view = vis.get_epoch_decision_view(EPOCH, RESOLUTION)
-
     grid = grid.reshape((-1, 2)).tolist()
     decision_view = decision_view * 255
     decision_view = decision_view.reshape((-1, 3)).astype(int).tolist()
-
-    color = vis.get_standard_classes_color() * 255
-    color = color.astype(int).tolist()
+    COLOR = vis.get_standard_classes_color() * 255
+    COLOR = COLOR.astype(int)
 
     # TODO fix its structure
     # evaluation = evaluator.get_eval(file_name="test_evaluation")
@@ -268,17 +259,12 @@ def update_projection():
     eval_new['ppr_train'] = 1
     eval_new['ppr_test'] = 1
 
-    label_color_list = []
-    label_list = []
-    for label in labels:
-        label_color_list.append(color[int(label)])
-        label_list.append(CLASSES[int(label)])
+    # maybe remove this? compute at frontend
+    label_color_list = COLOR[labels].tolist()
+    label_name_list = CLASSES[labels].tolist()
 
-    prediction_list = []
     prediction = data_provider.get_pred(EPOCH, all_data).argmax(-1)
-
-    for pred in prediction:
-        prediction_list.append(CLASSES[pred])
+    prediction_list = CLASSES[prediction].tolist()
     
     max_iter = (EPOCH_END - EPOCH_START) // EPOCH_PERIOD + 1
 
@@ -286,13 +272,9 @@ def update_projection():
     current_index = timevis.get_epoch_index(EPOCH)
 
     new_index = timevis.get_new_index(iteration)
-
     noisy_data = timevis.noisy_data_index()
-
     original_labels = timevis.get_original_labels()
-    original_label_list = []
-    for label in original_labels:
-        original_label_list.append(CLASSES[label])
+    original_label_list = CLASSES[original_labels].tolist()
 
     uncertainty_diversity_tot_dict = {}
     uncertainty_diversity_tot_dict['uncertainty'] = timevis.get_uncertainty_score(EPOCH)
@@ -322,7 +304,7 @@ def update_projection():
 
 
     return make_response(jsonify({'result': embedding_2d, 'grid_index': grid, 'grid_color': decision_view,
-                                  'label_color_list': label_color_list, 'label_list': label_list,
+                                  'label_color_list': label_color_list, 'label_list': label_name_list,
                                   'maximum_iteration': max_iter, 'training_data': current_index,
                                   'testing_data': testing_data_index, 'evaluation': eval_new,
                                   'prediction_list': prediction_list, 'new_selection': new_index,
