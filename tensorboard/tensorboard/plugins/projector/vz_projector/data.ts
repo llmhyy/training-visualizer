ae456ad8062fb145981fcc19338c61af451994d4
Yifan Zhang <38521787+Yvonnefanf@users.noreply.github.com>
2022-08-09 23:05:29 +0800

    retrain update bug

--- tensorboard/tensorboard/plugins/projector/vz_projector/data.ts
+++ tensorboard/tensorboard/plugins/projector/vz_projector/data.ts
@@ -780,6 +780,9 @@ export class DataSet {
       const hex = c.toString(16);
       return hex.length == 1 ? "0" + hex : hex;
     }
+        
+        
+    this.iterationChangeReset()
 
     function rgbToHex(r: number, g: number, b: number) {
       return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
